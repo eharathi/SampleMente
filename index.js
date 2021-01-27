2f5e9da48dad10d3d81ccc8c9c65b9c957f5bc5a
treddy <thoomu_vishnuvardhan_reddy@gap.com>
2021-01-27 20:57:26 +0530

    added DB

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
+const { Client } = require('pg');
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
 
 var app = express();
 
@@ -26,4 +37,21 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
+
+app.get('/', (request, response) => {
+	client.connect();
+let row1;
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+row1 = row;
+  }
+  client.end();
+});
+
+  response.json({ info: 'Node.js, Express, and Postgres API'})
+})
 app.listen(port);
+
+
